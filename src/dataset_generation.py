8d829e8378cbde5bf810c6a93a349b8d54012b04
Leonardo Vicentini <vicentini.leonardo99@gmail.com>
2022-11-20 22:31:05 +0100

    Update dataset_generation.py

--- src/dataset_generation.py
+++ src/dataset_generation.py
@@ -1,5 +1,7 @@
 import csv
 import uuid
+import random
+from utils import get_random_string, get_random_genre, get_random_int, get_random_float, get_random_year, get_random_country
 
 def generate_user_set():
     with open('../data/user_set.csv', 'w') as csvfile:
@@ -10,5 +12,21 @@ def generate_user_set():
             writer.writerow({'user_id': uuid.uuid4()})
     csvfile.close()
 
+def generate_relationl_table():
+    with open('../data/relational_table.csv', 'w') as csvfile:
+        fieldnames = ['name', 'genre', 'length', 'year', 'country', 'rating']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(100):
+            writer.writerow({'name': get_random_string(random.randint(5, 15)),
+                             'genre': get_random_genre(),
+                             'length': get_random_int(60, 200),
+                              'year': get_random_year(),
+                             'country': get_random_country(),
+                             'rating': get_random_float(0, 5)}
+                            )
+    csvfile.close()
+
 if __name__ == '__main__':
     generate_user_set()
+    generate_relationl_table()
