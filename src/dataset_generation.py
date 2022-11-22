070970af8e046cee7fd808d5d2bedd321342a7fd
lucavianunitn <72442491+lucavianunitn@users.noreply.github.com>
2022-11-22 15:39:08 +0100

    Added "complete" users and a first attempt to generate queries

--- src/dataset_generation.py
+++ src/dataset_generation.py
@@ -1,32 +1,187 @@
 import csv
 import uuid
 import random
-from utils import get_random_string, get_random_genre, get_random_int, get_random_float, get_random_year, get_random_country
+import os
+from utils import get_random_string, get_random_genre, get_random_int, get_random_float, get_random_year,  get_random_length, get_random_country
 
-def generate_user_set():
-    with open('../data/user_set.csv', 'w') as csvfile:
+def generate_user_set_old():
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, '../data/user_set.csv')
+
+    with open(filename, 'w') as csvfile:
         fieldnames = ['user_id']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for i in range(1000000):
+        for i in range(100):
             writer.writerow({'user_id': uuid.uuid4()})
     csvfile.close()
 
+def generate_user_set():
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, '../data/user_set.csv')
+    filename_complete = os.path.join(dir, '../data/complete_user_set.csv')
+
+    with open(filename, 'w') as csvfile:
+        with open(filename_complete, 'w') as csvfile_complete:
+
+            fieldnames = ['user_id']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            fieldnames_complete = ['user_id', 'preferred_genre_1', 'preferred_genre_2', 'less_preferred_genre', 'preferred_length', 'preferred_year', 'preferred_country_1', 'preferred_country_2', 'less_preferred_country', 'avarage_rating_translation']
+            writer_complete = csv.DictWriter(csvfile_complete, fieldnames=fieldnames_complete)
+            writer_complete.writeheader()
+
+            for i in range(100):
+                user_id = uuid.uuid4()
+                genres = get_random_genre(3)
+                countries = get_random_country(3)
+
+                writer.writerow({'user_id': user_id})
+
+                writer_complete.writerow({'user_id': user_id,
+                                        'preferred_genre_1': genres[0],
+                                        'preferred_genre_2': genres[1],
+                                        'less_preferred_genre': genres[2],
+                                        'preferred_length': get_random_length(1)[0],
+                                        'preferred_year': get_random_year(1)[0],
+                                        'preferred_country_1': countries[0],
+                                        'preferred_country_2': countries[1],
+                                        'less_preferred_country': countries[2],
+                                        'avarage_rating_translation': random.randint(-15, 15)
+                                        }
+                                        )
+
+        csvfile_complete.close()
+    csvfile.close()
+
 def generate_relational_table():
-    with open('../data/relational_table.csv', 'w') as csvfile:
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, '../data/relational_table.csv')
+
+    with open(filename, 'w') as csvfile:
         fieldnames = ['name', 'genre', 'length', 'year', 'country', 'rating']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for i in range(100):
             writer.writerow({'name': get_random_string(random.randint(5, 15)),
-                             'genre': get_random_genre(),
-                             'length': get_random_int(60, 200),
-                              'year': get_random_year(),
-                             'country': get_random_country(),
-                             'rating': get_random_float(0, 5)}
+                             'genre': get_random_genre(1)[0],
+                             'length': get_random_length(1)[0],
+                              'year': get_random_year(1)[0],
+                             'country': get_random_country(1)[0],
+                             'rating': get_random_int(0, 5)}
                             )
+    csvfile.close()
+
+def generate_queries():
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, '../data/queries.csv')
+    
+    counter=0
+
+    with open(filename, 'w') as csvfile:
+        fieldnames = ['query_id','genre', 'length', 'year', 'country']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        #Queries with 1 attribute
+        genres = get_random_genre(4) 
+        lengths=get_random_length(4)
+        years=get_random_year(4)
+        countries=get_random_country(6)
+
+        for i in range(4):
+            writer.writerow({'query_id': 'q'+str(counter),
+                            'genre': genres[i]}
+                            )
+            counter+=1
+
+        for i in range(4):
+            writer.writerow({'query_id': 'q'+str(counter),
+                            'length': lengths[i]}
+                            )
+            counter+=1
+
+        for i in range(4):
+            writer.writerow({'query_id': 'q'+str(counter),
+                            'year': years[i]}
+                            )
+            counter+=1
+
+        for i in range(6):
+            writer.writerow({'query_id': 'q'+str(counter),
+                            'country': countries[i]}
+                            )
+            counter+=1
+
+        #Queries with 2 attributes with genre
+        genres = get_random_genre(4) 
+
+        for i in range(4):
+            lengths=get_random_length(4)
+            years=get_random_year(4)
+            countries=get_random_country(6)
+
+            for j in range(4):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'genre': genres[i],
+                                'length': lengths[j]}
+                                )
+                counter+=1
+
+            for j in range(4):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'genre': genres[i],
+                                'year': years[j]}
+                                )
+                counter+=1
+
+            for j in range(6):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'genre': genres[i],
+                                'country': countries[j]}
+                                )
+                counter+=1
+
+        #Queries with 2 attributes with length
+        lengths=get_random_length(4)
+
+        for i in range(4):
+            years=get_random_year(4)
+            countries=get_random_country(6)
+
+            for j in range(4):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'length': lengths[i],
+                                'year': years[j]}
+                                )
+                counter+=1
+
+            for j in range(6):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'length': lengths[i],
+                                'country': countries[j]}
+                                )
+                counter+=1
+
+        
+        #Queries with 2 attributes with year
+        years=get_random_year(4)
+
+        for i in range(4):
+            countries=get_random_country(6)
+
+            for j in range(6):
+                writer.writerow({'query_id': 'q'+str(counter),
+                                'year': years[i],
+                                'country': countries[j]}
+                                )
+                counter+=1
+
     csvfile.close()
 
 if __name__ == '__main__':
     generate_user_set()
     generate_relational_table()
+    generate_queries()
+
--- src/utils.py
+++ src/utils.py
@@ -10,18 +10,23 @@ def get_random_float(a, b):
 def get_random_int(a, b):
     return random.randint(a, b)
 
-# get random year from 1900 to 2022
-def get_random_year():
-    return random.randint(1900, 2022)
+# get random decade from 1900 to 2022
+def get_random_year(n):
+    return random.sample(['1900','1910','1920','1930','1940','1950','1960','1970','1980','1990','2000','2010','2020'],n)
 
-def get_random_genre():
-    return random.choice(['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Sport', 'Thriller', 'War', 'Western'])
+# get random length from 60 to 200 minutes with a step of 15
+def get_random_length(n):
+    return random.sample(['60','75','90','105','120','135','150','165','180','195'],n)
+
+
+def get_random_genre(n):
+    return random.sample(['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Sport', 'Thriller', 'War', 'Western'],n)
 
 # get random choiche from ISO 3166-1 alpha-3
-def get_random_country():
-    return random.choice(['AFG', 'ALA', 'ALB', 'DZA', 'ASM', 'AND', 'AGO', 'AIA', 'ATA', 'ATG', 'ARG', 'ARM', 'ABW', 'AUS', 'AUT', 'AZE', 'BHS', 'BHR', 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ', 'BEN', 'BMU', 'BTN', 'BOL', 'BIH', 'BWA', 'BVT', 'BRA', 'IOT', 'BRN', 'BGR', 'BFA', 'BDI', 'KHM', 'CMR', 'CAN', 'CPV', 'CYM', 'CAF', 'TCD', 'CHL', 'CHN', 'CXR', 'CCK', 'COL', 'COM', 'COG', 'COD', 'COK', 'CRI', 'CIV', 'HRV', 'CUB', 'CYP', 'CZE', 'DNK', 'DJI',
+def get_random_country(n):
+    return random.sample(['AFG', 'ALA', 'ALB', 'DZA', 'ASM', 'AND', 'AGO', 'AIA', 'ATA', 'ATG', 'ARG', 'ARM', 'ABW', 'AUS', 'AUT', 'AZE', 'BHS', 'BHR', 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ', 'BEN', 'BMU', 'BTN', 'BOL', 'BIH', 'BWA', 'BVT', 'BRA', 'IOT', 'BRN', 'BGR', 'BFA', 'BDI', 'KHM', 'CMR', 'CAN', 'CPV', 'CYM', 'CAF', 'TCD', 'CHL', 'CHN', 'CXR', 'CCK', 'COL', 'COM', 'COG', 'COD', 'COK', 'CRI', 'CIV', 'HRV', 'CUB', 'CYP', 'CZE', 'DNK', 'DJI',
                           'DMA', 'DOM', 'ECU', 'EGY', 'SLV', 'GNQ', 'ERI', 'EST', 'ETH', 'FLK', 'FRO', 'FJI', 'FIN', 'FRA', 'GUF', 'PYF', 'ATF', 'GAB', 'GMB', 'GEO', 'DEU', 'GHA', 'GIB', 'GRC', 'GRL', 'GRD', 'GLP', 'GUM', 'GTM', 'GGY', 'GIN', 'GNB', 'GUY', 'HTI', 'HMD', 'VAT', 'HND', 'HKG', 'HUN', 'ISL', 'IND', 'IDN', 'IRN', 'IRQ', 'IRL', 'IMN', 'ISR', 'ITA', 'JAM', 'JPN', 'JEY', 'JOR', 'KAZ', 'KEN', 'KIR', 'PRK', 'KOR', 'KWT', 'KGZ', 'LAO',
                           'LVA', 'LBN', 'LSO', 'LBR', 'LBY', 'LIE', 'LTU', 'LUX', 'MAC', 'MKD', 'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MLT', 'MHL', 'MTQ', 'MRT', 'MUS', 'MYT', 'MEX', 'FSM', 'MDA', 'MCO', 'MNG', 'MNE', 'MSR', 'MAR', 'MOZ', 'MMR', 'NAM', 'NRU', 'NPL', 'NLD', 'ANT', 'NCL', 'NZL', 'NIC', 'NER', 'NGA', 'NIU', 'NFK', 'MNP', 'NOR', 'OMN', 'PAK', 'PLW', 'PSE', 'PAN', 'PNG', 'PRY', 'PER', 'PHL', 'PCN', 'POL', 'PRT', 'PRI', 'QAT', 'REU',
                           'ROU', 'RUS', 'RWA', 'BL', 'SHN', 'KNA', 'LCA', 'MAF', 'SPM', 'VCT', 'WSM', 'SMR', 'STP', 'SAU', 'SEN', 'SRB', 'SYC', 'SLE', 'SGP', 'SVK', 'SVN', 'SLB', 'SOM', 'ZAF', 'SGS', 'SSD', 'ESP', 'LKA', 'SDN', 'SUR', 'SJM', 'SWZ', 'SWE', 'CHE', 'SYR', 'TWN', 'TJK', 'TZA', 'THA', 'TLS', 'TGO', 'TKL', 'TON', 'TTO', 'TUN', 'TUR', 'TKM', 'TCA', 'TUV', 'UGA', 'UKR', 'ARE', 'GBR', 'USA', 'UMI', 'URY', 'UZB', 'VUT', 'VEN', 'VNM',
-                          'VGB', 'VIR', 'WLF', 'ESH', 'YEM', 'ZMB', 'ZWE'])
+                          'VGB', 'VIR', 'WLF', 'ESH', 'YEM', 'ZMB', 'ZWE'],n)
 
