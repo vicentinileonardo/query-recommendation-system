70e2817698be27157078a86fd85b3f3b5e0c752d
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-01-10 18:57:14 +0100

    fixing issue #01 related to empty rows

---
+++ src/notes.md
@@ -0,0 +1,4 @@
+## Issue #01
+problem when the query row has as onlu 0s as rating availables
+
+for q64, good ratings calculated
--- src/strategy_test.py
+++ src/strategy_test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+import random
 
 # set pandas to print the entire dataframe when printing
 pd.set_option('display.max_columns', None)
@@ -8,12 +9,9 @@ pd.set_option('display.max_rows', None)
 
 utility_matrix = pd.read_csv('../data/_utility_matrix.csv', index_col=0)
 
-
 utility_matrix = utility_matrix.T
-utility_matrix = utility_matrix.iloc[:55, :100]
+utility_matrix = utility_matrix.iloc[:89, :50]
 utility_matrix = utility_matrix.fillna(np.nan)
-
-
 
 '''
 dummy_um = pd.DataFrame([[1, np.nan, 3, np.nan, np.nan, 5, np.nan, np.nan, 5, np.nan, 4, np.nan],
@@ -23,19 +21,26 @@ dummy_um = pd.DataFrame([[1, np.nan, 3, np.nan, np.nan, 5, np.nan, np.nan, 5, np
                        [np.nan, np.nan, 4, 3, 4, 2, np.nan, np.nan, np.nan, np.nan, 2, 5],
                        [1, np.nan, 3, np.nan, 3, np.nan, np.nan, 2, np.nan, np.nan, 4, np.nan]])
 
-
 dummy_um.index = utility_matrix.index[:6]
 dummy_um.columns = utility_matrix.columns[:12]
-
 utility_matrix = dummy_um
-print(utility_matrix)
-
 '''
 
-# order the rows, using row.count to count the most non nan values
-utility_matrix = utility_matrix.reindex(utility_matrix.count(axis=1).sort_values(ascending=False).index)
+#print("1, utility matrix:\n")
 #print(utility_matrix)
 
+# order the rows, using row.count to count the most non nan values
+#utility_matrix = utility_matrix.reindex(utility_matrix.count(axis=1).sort_values(ascending=False).index)
+
+#print(utility_matrix)
+
+test_utility_matrix = utility_matrix.copy()
+
+# Addressing issue #01 check if row mean == 0 (only 0s and NAs), if that is true,  change the 0.0s with 1.0s
+for query in range(utility_matrix.shape[0]):
+    if utility_matrix.iloc[query, :].mean() == 0:
+        for user in range(utility_matrix.shape[1]):
+            utility_matrix.iloc[query, user] = round((random.randint(1, 100) * utility_matrix.iloc[:, user].mean()) % 10)
 
 # Subtract the mean of each row (query) from the ratings
 centered_matrix = utility_matrix.copy()
@@ -44,8 +49,23 @@ for row in range(utility_matrix.shape[0]):
         if utility_matrix.iloc[row, col] != np.nan:
             centered_matrix.iloc[row, col] = utility_matrix.iloc[row, col] - utility_matrix.iloc[row, :].mean()
 
-centered_matrix = centered_matrix.fillna(0)
+#Note: merge the above loops into one
 
+#print("2, centered matrix, before:\n")
+#print(centered_matrix)
+
+
+
+# filling NAs of the centered matrix
+# option 1: fill na with 0s
+#centered_matrix = centered_matrix.fillna(0)
+
+# option 2: fill na cells with the mean of their row (query)
+centered_matrix = centered_matrix.apply(lambda row: row.fillna(row.mean()), axis=1)
+
+
+#print("3, centered matrix, after:\n")
+#print(centered_matrix)
 
 def calculate_rating(utility_matrix, counter, user=4, query=0, top_n=2):
     # cosine similarity between rows
@@ -65,27 +85,48 @@ def calculate_rating(utility_matrix, counter, user=4, query=0, top_n=2):
 
     rating = 0
     for i in top_n_similarities:
-
-        # handle nan values, ref. slide 32 of RecSys slide deck
+        # handle nan values, ref. slide 32 of RecSys slide deck, using the mean of the query
         if np.isnan(utility_matrix.iloc[i, user]):
             rating += similarities[i] * utility_matrix.iloc[i, :].mean()
         else:
             rating += similarities[i] * utility_matrix.iloc[i, user]
     #print(rating)
     print('sum', similarities[top_n_similarities].sum())
+
     # handle division by zero
     if similarities[top_n_similarities].sum() == 0:
         print('inside exception')
+
         # option 1: return the mean of the query
         rating = utility_matrix.iloc[query, :].mean()
+
         # option 2: return the mean of the user
         # rating = utility_matrix.iloc[:, user].mean()
+
+        # option 3
+        #rating = 0
+
         counter["c_exception"] += 1
-    else:
+    else: # normal behaviour
         rating /= similarities[top_n_similarities].sum()
         counter["c_normal"] += 1
     rating = round(rating)
+
+    if rating > 100:
+        rating = 100
+    elif rating < 0:
+        rating = 0
+
     return rating, counter
+
+'''
+# test
+counter = {"c_exception": 0, "c_normal": 0}
+test_rating, test_counter = calculate_rating(utility_matrix, counter, user=4, query=0, top_n=2)
+print('test rating:', test_rating)
+print('test counter:', test_counter)
+
+'''
 
 # loop all the cell of utility matrix with nan and calculate the rating
 complete_utility_matrix = utility_matrix.copy()
@@ -102,16 +143,15 @@ for row in range(utility_matrix.shape[0]):
             # append the rating also to a list
             ratings_list.append(rating)
 
-print(ratings_list)
-print(utility_matrix)
-print(complete_utility_matrix)
+print('rating_list:', ratings_list)
+#print(utility_matrix)
+#print(complete_utility_matrix)
 print('counter exception:', counter)
 
 
 with open('../data/viz.html', 'w') as f:
     f.write(utility_matrix.to_html())
     f.write(complete_utility_matrix.to_html())
-
 
 
 
