428e4522aae249a4211ad889679c4e17d0330632
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-01-13 18:44:29 +0100

    general bug fixing

--- src/notes.md
+++ src/notes.md
@@ -8,3 +8,19 @@
 
 
 for q64, good ratings calculated (if an example to check is needed)
+
+## Clustering
+Clustering can be useful as a pre-processing step to improve the performance of the CF algorithm. 
+Clustering can be used to group similar items together and then the similarity between items within the same cluster can be considered stronger than the similarity between items in different clusters. 
+This can help to reduce the dimensionality of the problem and make the CF algorithm more computationally efficient.
+
+Additionally, it can also be used as post-processing step, 
+once the similarity matrix is calculated, 
+you could group similar items together in clusters and then recommend the items within the same cluster, 
+this will help to improve the diversity of the recommendations.
+
+The usefulness of clustering in item-item CF depends on the specific characteristics of the dataset and the problem you are trying to solve, 
+so it's worth to test the performance of the algorithm with and without clustering and see which one performs better.
+
+## Issue #03
++ MRE is not working properly
--- src/strategy_test.py
+++ src/strategy_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
 import random
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -23,6 +24,10 @@ def import_data(matrix_type="partial"):
     utility_matrix = pd.read_csv(path, index_col=0)
     return utility_matrix
 
+def prepare_matrix(matrix, n_queries=100, n_users=100):
+    matrix = matrix.T
+    matrix = matrix.iloc[:n_queries, :n_users]
+    return matrix
 def preprocess_data(utility_matrix, n_queries=100, n_users=100):
     utility_matrix = utility_matrix.copy()
     utility_matrix = utility_matrix.T
@@ -48,21 +53,23 @@ def preprocess_data(utility_matrix, n_queries=100, n_users=100):
 
     utility_matrix_before_pp = utility_matrix.copy()
 
-    # Addressing issue #01 check if row mean == 0 (only 0s and NAs), if that is true,  change the 0.0s with something else
+    centered_matrix = utility_matrix.copy()
+
+    # Addressing issue #01 check if row mean == 0 (only 0s and NAs), if that is true, change the 0.0s with something else
     for query in range(utility_matrix.shape[0]):
         if utility_matrix.iloc[query, :].mean() == 0:
             for user in range(utility_matrix.shape[1]):
                 if utility_matrix.iloc[query, user] == 0:  # 0s, not NAs
-                    utility_matrix.iloc[query, user] = round((1 * utility_matrix.iloc[:, user].mean()) % 10) # to fix, not use the modulo
-                    #utility_matrix.iloc[query, user] = 5
+                    #utility_matrix.iloc[query, user] = round((utility_matrix.iloc[:, user].mean()) / 10)
+                    # think to do this step directly onto the centered matrix, without touching the original one
+                    centered_matrix.iloc[query, user] = round((utility_matrix.iloc[:, user].mean()) / 10)
 
     # Centered matrix: subtract the mean of each row (query) from the ratings
-    centered_matrix = utility_matrix.copy()
+
     for row in range(utility_matrix.shape[0]):
         for col in range(utility_matrix.shape[1]):
             if utility_matrix.iloc[row, col] != np.nan:
                 centered_matrix.iloc[row, col] = utility_matrix.iloc[row, col] - utility_matrix.iloc[row, :].mean()
-
 
 
     #Note: merge the above loops into one
@@ -77,23 +84,49 @@ def preprocess_data(utility_matrix, n_queries=100, n_users=100):
 
     partial_utility_matrix = utility_matrix.copy()
 
+    ### CLUSTERING STEP ###
+    '''
+    # perform a k-means clustering on the centered matrix
+    # option 1: use the euclidean distance
+    kmeans = KMeans(n_clusters=5, random_state=0, n_init=10).fit(centered_matrix)
+
+    # option 2: use the cosine similarity
+    #kmeans = KMeans(n_clusters=5, random_state=0, metric='cosine').fit(centered_matrix)
+
+    # use fit_predict to get the cluster labels
+    cluster_labels = kmeans.fit_predict(centered_matrix)
+
+    centered_matrix['cluster'] = cluster_labels
+
+    grouped_matrix = centered_matrix.groupby('cluster').mean()
+
+    print('centered_matrix \n')
+    print(centered_matrix['cluster'])
+
+    print('grouped_matrix \n')
+    print(grouped_matrix)
+    '''
+
     return utility_matrix_before_pp, partial_utility_matrix, centered_matrix
 
-def calculate_rating(utility_matrix, centered_matrix, counter, user=4, query=0, top_n=2):
+def calculate_rating(utility_matrix, centered_matrix, counter, user=4, query=0, top_n=2, verbose=False):
     # cosine similarity between rows
     similarities = cosine_similarity(centered_matrix, centered_matrix)
 
     # selecting the column related to the similarities of the specific query against the others
     similarities = similarities[query, :]
-    print('similarities:', similarities)
+    if verbose:
+        print('Similarities:', similarities)
 
     # Sort the similarities in descending order
     sorted_similarities = np.argsort(similarities)[::-1]
-    print('sorted_sim:', sorted_similarities)
+    if verbose:
+        print('Sorted similarities:', sorted_similarities)
 
     # Select the top N similar queries, excluding the query itself
     top_n_similarities = sorted_similarities[1:top_n+1]
-    print('top similarities: ', top_n_similarities)
+    if verbose:
+        print('Top N similarities:', top_n_similarities)
 
     rating = 0
     for i in top_n_similarities:
@@ -130,22 +163,31 @@ def calculate_rating(utility_matrix, centered_matrix, counter, user=4, query=0, 
 
     return rating, counter
 
-def collaborative_filtering(utility_matrix, centered_matrix, top_n=2):
+def collaborative_filtering(utility_matrix, centered_matrix, top_n=2, verbose=False):
     # loop all the cell of utility matrix with nan and calculate the rating
     complete_utility_matrix = utility_matrix.copy()
-    print('Complete matrix shape:', complete_utility_matrix.shape)
+    if verbose:
+        print('Complete matrix shape:', complete_utility_matrix.shape)
     ratings_list = []
     counter = {"c_exception": 0, "c_normal": 0}
+
     for row in range(utility_matrix.shape[0]):
         for col in range(utility_matrix.shape[1]):
             if np.isnan(utility_matrix.iloc[row, col]):
-                print('row:', row, 'col:', col)
-                rating, counter_exception = calculate_rating(utility_matrix, centered_matrix, counter, user=col, query=row, top_n=top_n)
+                if verbose:
+                    print('Row:', row, 'Col:', col)
+                rating, counter_exception = calculate_rating(utility_matrix, centered_matrix, counter, user=col, query=row, top_n=top_n, verbose=verbose)
                 complete_utility_matrix.iloc[row, col] = rating
                 # append the rating also to a list
                 ratings_list.append(rating)
-        print('Row: ', row, 'Estimated percentage of completion: ', round((row / utility_matrix.shape[0]) * 100, 2), '%')
-    print('Counter: ', counter)
+
+                # rating2, calculated with the clustering
+
+        print('Query: ', row, 'Estimated percentage of completion: ', round((row / utility_matrix.shape[0]) * 100, 2), '%')
+
+    if verbose:
+        print('Counter: ', counter)
+
     return complete_utility_matrix, ratings_list
 
 def compute_difference(utility_matrix_complete, n_queries=100, n_users=100):
@@ -159,58 +201,23 @@ def compute_difference(utility_matrix_complete, n_queries=100, n_users=100):
 
 # testing performances
 def calculate_mae(real_utility_matrix_complete, utility_matrix_complete):
-    # calculate the MAE
-    mae = 0
-    for row in range(utility_matrix_complete.shape[0]):
-        for col in range(utility_matrix_complete.shape[1]):
-            if np.isnan(utility_matrix_complete.iloc[row, col]):
-                mae += abs(real_utility_matrix_complete.iloc[row, col] - utility_matrix_complete.iloc[row, col])
-    mae /= utility_matrix_complete.isna().sum().sum()
+    mae = mean_absolute_error(real_utility_matrix_complete, utility_matrix_complete)
     return mae
 
-    # alternative, to be tested
-    # mae = mean_absolute_error(real_utility_matrix_complete, complete_utility_matrix)
-
 def calculate_rmse(real_utility_matrix_complete, utility_matrix_complete):
-    # calculate the RMSE
-    rmse = 0
-    for row in range(utility_matrix_complete.shape[0]):
-        for col in range(utility_matrix_complete.shape[1]):
-            if np.isnan(utility_matrix_complete.iloc[row, col]):
-                rmse += (real_utility_matrix_complete.iloc[row, col] - utility_matrix_complete.iloc[row, col]) ** 2
-    rmse /= utility_matrix_complete.isna().sum().sum()
-    rmse = math.sqrt(rmse)
+    rmse = math.sqrt(mean_squared_error(real_utility_matrix_complete, utility_matrix_complete))
     return rmse
 
-    # alternative, to be tested
-    # rmse = sqrt(mean_squared_error(real_utility_matrix_complete, complete_utility_matrix))
+# epsilon smoothing due to a runtime error in dividing by zero or by a very small number
+def calculate_mape(real_utility_matrix_complete, utility_matrix_complete, epsilon=1e-8):
+    actual, predicted = np.array(real_utility_matrix_complete), np.array(utility_matrix_complete)
+    mask = actual != 0
+    return (np.fabs(actual - predicted)/(actual + epsilon))[mask].mean() * 100
 
-def calculate_mape(real_utility_matrix_complete, utility_matrix_complete):
-    # calculate the MAPE
-    mape = 0
-    for row in range(utility_matrix_complete.shape[0]):
-        for col in range(utility_matrix_complete.shape[1]):
-            if np.isnan(utility_matrix_complete.iloc[row, col]):
-                mape += abs(real_utility_matrix_complete.iloc[row, col] - utility_matrix_complete.iloc[row, col]) / real_utility_matrix_complete.iloc[row, col]
-    mape /= utility_matrix_complete.isna().sum().sum()
-    mape *= 100
-    return mape
-
-    # alternative, to be tested
-    # mape = np.mean(np.abs((real_utility_matrix_complete - complete_utility_matrix) / real_utility_matrix_complete)) * 100
-
-def calculate_mre(real_utility_matrix_complete, utility_matrix_complete):
-    # calculate the MRE
-    mre = 0
-    for row in range(utility_matrix_complete.shape[0]):
-        for col in range(utility_matrix_complete.shape[1]):
-            if np.isnan(utility_matrix_complete.iloc[row, col]):
-                mre += abs(real_utility_matrix_complete.iloc[row, col] - utility_matrix_complete.iloc[row, col]) / real_utility_matrix_complete.iloc[row, col]
-    mre /= utility_matrix_complete.isna().sum().sum()
-    return mre
-
-    # alternative, to be tested
-    # mre = np.mean(np.abs((real_utility_matrix_complete - complete_utility_matrix) / real_utility_matrix_complete))
+def calculate_mre(real_utility_matrix_complete, utility_matrix_complete, epsilon=1e-8):
+    actual, predicted = np.array(real_utility_matrix_complete), np.array(utility_matrix_complete)
+    mask = actual != 0
+    return (np.fabs(actual - predicted)/(actual + epsilon))[mask].mean()
 
 def print_data_to_html(utility_matrix_before_pp, partial_utility_matrix, centered_matrix, utility_matrix_complete, difference_utility_matrix):
     with open('../data/viz.html', 'w') as f:
@@ -227,22 +234,20 @@ def print_data_to_html(utility_matrix_before_pp, partial_utility_matrix, centere
         f.write(difference_utility_matrix.to_html())
 
 # function that plot the heatmap of the utility matrix
-def plot_heatmap(utility_matrix, n_rows, n_columns, title):
+def plot_heatmap(utility_matrix, title, annot=True, n_rows=100, n_columns=100):
     utility_matrix = utility_matrix.copy()
     utility_matrix = utility_matrix.iloc[:n_rows, :n_columns]
-    plt.figure(figsize=(12,10), dpi=75)
-    sns.heatmap(utility_matrix, cmap='coolwarm', center=0, annot=True)
+    plt.figure(figsize=(12,16), dpi=75)
+    sns.heatmap(utility_matrix, cmap='coolwarm', center=0, annot=annot)
 
     # Decorate
     plt.title(title, fontsize=22)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
 
-
     # save the plot
     plt.savefig('../data/heatmap.png')
     plt.show()
-
 
 def get_top_k_queries(partial_utility_matrix, utility_matrix_complete, user=0, top_k=10):
     if partial_utility_matrix.shape != utility_matrix_complete.shape:
@@ -262,7 +267,6 @@ def get_top_k_queries(partial_utility_matrix, utility_matrix_complete, user=0, t
     # return the top N queries
     return query_rating_list[:top_k]
 
-
 def save_top_k_queries(partial_utility_matrix, utility_matrix_complete, top_k=10, n_users=10):
     path = '../data/top_' + str(top_k) + '_queries_n_' + str(n_users) + '_users.txt'
 
@@ -275,6 +279,12 @@ def save_top_k_queries(partial_utility_matrix, utility_matrix_complete, top_k=10
 
 if __name__ == '__main__':
 
+    N_QUERIES = 100
+    N_USERS = 350
+    TOP_N = 2
+    TOP_K_QUERIES = 10
+    N_USERS_TOP_K_QUERIES = 10
+
     # load the data
     start_time_load = time.time()
     partial_utility_matrix = import_data('partial')
@@ -283,37 +293,43 @@ if __name__ == '__main__':
 
     # preprocess the data
     start_time_pp = time.time()
-    utility_matrix_before_pp, partial_utility_matrix, centered_matrix = preprocess_data(partial_utility_matrix, n_queries=100, n_users=200)
+    utility_matrix_before_pp, partial_utility_matrix, centered_matrix = preprocess_data(partial_utility_matrix, n_queries=N_QUERIES, n_users=N_USERS)
     end_time_pp = time.time()
     print('Time taken to preprocess the partial utility matrix:', end_time_pp - start_time_pp)
 
     # complete the utility matrix with the ratings
     start_time_cf = time.time()
-    utility_matrix_complete, ratings_list = collaborative_filtering(partial_utility_matrix, centered_matrix, top_n=2)
+    utility_matrix_complete, ratings_list = collaborative_filtering(partial_utility_matrix, centered_matrix, top_n=TOP_N)
     end_time_cf = time.time()
     print('Time taken to fill the matrix with CF:', end_time_cf - start_time_cf)
 
-    difference_utility_matrix = compute_difference(utility_matrix_complete, n_queries=85, n_users=85)
+    difference_utility_matrix = compute_difference(utility_matrix_complete, n_queries=N_QUERIES, n_users=N_USERS)
 
     # print the data to html, for testing purposes
     print_data_to_html(utility_matrix_before_pp, partial_utility_matrix, centered_matrix, utility_matrix_complete, difference_utility_matrix)
 
-
     # plot the heatmap of the difference utility matrix
-    plot_heatmap(difference_utility_matrix, 50, 50, 'Difference')
+    plot_heatmap(difference_utility_matrix, 'Difference', annot=False, n_rows=N_QUERIES, n_columns=N_USERS)
 
-    '''
-    # calculate the performances
-    mae = calculate_mae(utility_matrix_complete, utility_matrix_complete)
-    rmse = calculate_rmse(utility_matrix_complete, utility_matrix_complete)
-    mape = calculate_mape(utility_matrix_complete, utility_matrix_complete)
-    mre = calculate_mre(utility_matrix_complete, utility_matrix_complete)
+    real_utility_matrix_complete = import_data('real_complete')
+    real_utility_matrix_complete = prepare_matrix(real_utility_matrix_complete, n_queries=N_QUERIES, n_users=N_USERS)
 
+    # calculate and printing the performances
+    print('\033[1m' + 'Performance of the collaborative filtering algorithm:' + '\033[0m')
+
+    # mean absolute error: might be helped by the correct prediction of the 0s
+    mae = calculate_mae(real_utility_matrix_complete, utility_matrix_complete)
     print('MAE: ', mae)
-    print('RMSE: ', rmse)
-    print('MAPE: ', mape)
+
+    # RMSE is sensitive to outliers, since the square operation magnifies larger errors.
+    rmse = calculate_rmse(real_utility_matrix_complete, utility_matrix_complete)
+    print('RMSE :', rmse)
+
+    mape = calculate_mape(real_utility_matrix_complete, utility_matrix_complete)
+    print('MAPE :', mape)
+
+    mre = calculate_mre(utility_matrix_complete, utility_matrix_complete)
     print('MRE: ', mre)
-    '''
 
     # save the top k queries
-    save_top_k_queries(partial_utility_matrix, utility_matrix_complete, top_k=10, n_users=10)
+    save_top_k_queries(partial_utility_matrix, utility_matrix_complete, top_k=TOP_K_QUERIES, n_users=N_USERS_TOP_K_QUERIES)
